b4fe677c80fbb80f5a2fb9c3bbfe3bb74e601dac
hulizong <a1207988466@qq.com>
2019-09-18 17:42:33 +0800

    123

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -18,7 +18,7 @@ namespace WebApplication2
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args).UseUrls("http://*:5000")
                 .UseStartup<Startup>();
     }
 }
